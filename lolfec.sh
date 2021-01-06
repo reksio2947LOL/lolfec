928aae292ac16a46436da056a1c8de934edd8acd
reksio2947LOL <77068195+reksio2947LOL@users.noreply.github.com>
2021-01-06 19:52:25 +0100

    Add files via upload

--- lolfec.sh
+++ lolfec.sh
@@ -9,8 +9,8 @@ sh=$(basename $SHELL)
 uptime=$(uptime -p)
 
 
-echo -e "\033[1;31m .      . . ..  .  \033[1;35m$me\033[0m@\033[1;35m$host"
-echo -e "\033[1;33m .      .    .  .   \033[1;35mOS: \033[0m$os"
-echo -e "\033[1;32m .      .    .  .    \033[1;35mKernel: \033[0m$kernel"
-echo -e "\033[1;34m .      .    .  .     \033[1;35mShell: \033[0m$sh"
-echo -e "\033[1;35m ...... ......  ...... \033[1;35mUptime: \033[0m$uptime"
+echo -e "\033[1;96m .      . . ..  .     \033[1;32m$me\033[0m@\033[1;35m$host"
+echo -e "\033[1;96m .      .    .  .     \033[1;32mOS: \033[0m$os"
+echo -e "\033[1;96m .      .    .  .     \033[1;32mKernel: \033[0m$kernel"
+echo -e "\033[1;96m .      .    .  .     \033[1;32mShell: \033[0m$sh"
+echo -e "\033[1;96m ...... ......  ......\033[1;32mUptime: \033[0m$uptime"
