d71326517fc0db65e111265083ecfee4e6ba4f86
reksio2947LOL <77068195+reksio2947LOL@users.noreply.github.com>
2021-01-07 10:54:49 +0100

    Zmieniono kolory

--- lolfec.sh
+++ lolfec.sh
@@ -9,8 +9,8 @@ sh=$(basename $SHELL)
 uptime=$(uptime -p)
 
 
-echo -e "\033[1;96m .      . . ..  .     \033[1;32m$me\033[0m@\033[1;35m$host"
-echo -e "\033[1;96m .      .    .  .     \033[1;32mOS: \033[0m$os"
-echo -e "\033[1;96m .      .    .  .     \033[1;32mKernel: \033[0m$kernel"
-echo -e "\033[1;96m .      .    .  .     \033[1;32mShell: \033[0m$sh"
-echo -e "\033[1;96m ...... ......  ......\033[1;32mUptime: \033[0m$uptime"
+echo -e "\033[1;31m ...    . . ..  ...\033[1;96m$me\033[0m@\033[1;35m$host"
+echo -e "\033[1;33m ...    ......  ... \033[1;96mOS: \033[0m$os"
+echo -e "\033[1;32m ...    ..  ..  ...  \033[1;96mKernel: \033[0m$kernel"
+echo -e "\033[1;34m ...... ......  ......\033[1;96mShell: \033[0m$sh"
+echo -e "\033[1;35m ...... ......  ...... \033[1;96mUptime: \033[0m$uptime"
